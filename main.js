f5549ed2945d377958cd23111fd1bdec19d25381
bunny4byss <bunny4byss@gmail.com>
2018-09-01 01:11:20 -0300

    JavaScript

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 // JavaScript source code
-alert("Frase");
+function compute() {
+	total = 3
+	score = 0
+	if (document.getElementById("q1b").checked){
+		score = score+1
+	}
+	if (document.getElementById("q2b").checked){
+		score = score+1
+	}
+	if (document.getElementById("q3a").checked){
+		score = score+1
+	}
+	if (score == 3){
+		alert("Parabens, voce acertou "+ score+" de "+ total+ " perguntas")
+	}
+	else{
+	    alert("Voce acertou "+ score+" de "+ total+ " perguntas")
+	}
+}
